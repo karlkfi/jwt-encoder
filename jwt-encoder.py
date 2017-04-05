30f8383359b6bf7c5759c46f1efda632e65465eb
Karl Isenberg <karl@mesosphere.io>
2017-04-04 19:17:15 -0700

    Change uid & private-key-path to arguments

--- jwt-encoder.py
+++ jwt-encoder.py
@@ -5,15 +5,14 @@ import jwt
 import time
 
 @click.command()
-@click.option('--uid', help='Unique ID of the user.', required=True)
-@click.option('--private-key-path', help='File path of the auth token secret.', required=True)
+@click.argument('uid', help='Unique ID of the user.', required=True)
+@click.argument('private-key-path', help='File path of the auth token secret.', required=True, type=click.File('r'))
 @click.option('--duration', default=86400, help='Time until token expiration, in seconds.')
 @click.option('--algorithm', default='HS256', help='Algorithm to encode with.')
-def cli(private_key_path, uid, duration, algorithm):
+def cli(uid, private_key_file, duration, algorithm):
     expTime = time.time() + (3600 * duration)
-    file = open(private_key_path, 'r')
-    secret_key = file.read()
-    token = jwt.encode({'exp': expTime, 'uid': uid}, secret_key, algorithm=algorithm)
+    private_key = private_key_file.read()
+    token = jwt.encode({'exp': expTime, 'uid': uid}, private_key, algorithm=algorithm)
     click.echo(token)
 
 cli()
